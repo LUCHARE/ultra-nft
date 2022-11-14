e3e2ac6203d7c92ef74244b5b4a59084d020add9
Dmitry <luchare.dev@gmail.com>
2022-11-14 16:54:57 +1000

    Добработан сборщик, начата работа со стилями В gulp добавления задача dev, которая запускает локальный сервер. Написаны стили для шапки и первых двух блоков (без адаптации)

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -4,14 +4,16 @@ import dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 import sourcemaps from 'gulp-sourcemaps'
 import gulpif from 'gulp-if'
+import browserSync from 'browser-sync'
 import { deleteAsync as del } from 'del'
 import dotenv from 'dotenv'
 
-const {src, dest, series, parallel} = gulp
+const {src, dest, watch, series, parallel} = gulp
 
 dotenv.config()
 
 const sass = gulpSass(dartSass)
+const reload = browserSync.reload
 
 const path = {
     src: {
@@ -29,6 +31,13 @@ const path = {
         scripts: 'dist/scripts/',
         images: 'dist/images/',
         fonts: 'dist/fonts/'
+    },
+    watch: {
+        markup: 'src/**/*.html',
+        styles: ['src/styles/**/*.css','src/styles/**/*.scss'],
+        scripts: ['src/scripts/**/*.js', 'src/scripts/**/*.cjs', 'src/scripts/**/*.mjs'],
+        images: 'src/images/**/*.*',
+        fonts: 'src/fonts/**/*.*'
     }
 }
 
@@ -37,14 +46,16 @@ const development = process.env['NODE_ENV'] == 'development'
 function buildStyles() {
     return src(path.src.styles)
         .pipe(gulpif(development, sourcemaps.init()))
-        .pipe(sass.sync({}).on('error', err => console.log(err)))
+        .pipe(sass.sync({}).on('error', sass.logError))
         .pipe(gulpif(development, sourcemaps.write('./')))
         .pipe(dest(path.dist.styles))
+        .pipe(reload({ stream: true }))
 }
 
 function buildMarkup() {
     return src(path.src.markup)
         .pipe(dest(path.dist.markup))
+        .pipe(reload({ stream: true }))
 }
 
 function buildScripts() {
@@ -52,16 +63,19 @@ function buildScripts() {
         .pipe(sourcemaps.init())
         .pipe(dest(path.dist.scripts))
         .pipe(sourcemaps.write('./'))
+        .pipe(reload({ stream: true }))
 }
 
 function buildImages() {
     return src(path.src.images)
         .pipe(dest(path.dist.images))
+        .pipe(reload({ stream: true }))
 }
 
 function buildFonts() {
     return src(path.src.fonts)
         .pipe(dest(path.dist.fonts))
+        .pipe(reload({ stream: true }))
 }
 
 function clean() {
@@ -70,7 +84,13 @@ function clean() {
 
 // TODO
 function dev() {
+    browserSync({server: 'dist', ghostMode: false})
 
+    watch(path.watch.markup, { ignoreInitial: false }, buildMarkup)
+    watch(path.watch.styles, { ignoreInitial: false }, buildStyles)
+    watch(path.watch.scripts, { ignoreInitial: false }, buildScripts)
+    watch(path.watch.images, { ignoreInitial: false }, buildImages)
+    watch(path.watch.fonts, { ignoreInitial: false }, buildFonts)
 }
 
 const build = series(clean, parallel(buildMarkup, buildStyles, buildScripts, buildImages, buildFonts))
